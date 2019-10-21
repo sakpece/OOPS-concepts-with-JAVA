622742928ee5394ff254e17da268fadf2114cb38
sakpece <sakpece@gmail.com>
2019-10-21 11:59:47 +0530

    Animal base class

---
+++ Animal.java
@@ -0,0 +1,16 @@
+
+class Animal {
+String name ;
+public String sound()
+{
+ return "I have no suond please implement in my child class "; 
+}
+public void setName(String name)
+{
+ this.name= name; 
+}
+public String getName()
+{
+ return this.name; 
+} 
+}
